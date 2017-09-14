25bd43eeb5011fe4ae410c728363bf2633d93f5a
kenoteb <kenoteb@onid.oregonstate.edu>
2017-09-14 14:39:13 -0700

    scrollable bank feed

--- src/components/BankFeed.vue
+++ src/components/BankFeed.vue
@@ -1,50 +1,31 @@
 <template>
   <div class="bank-feed">
     <h3>Bank Feed</h3>
-    <router-link v-bind:to="'/banks/' + bank._id" v-for="bank in pagedBanks" v-bind:key="bank._id">
-      <div class="bank-slip">
-        <p>{{ bank.memberName }}</p>
-        <p><span class="slip-label">Title: </span>{{ bank.title }}</p>
-        <p><span class="slip-label">Issued On: </span>{{ bank.dateCreated }}</p>
-      </div>
-    </router-link>
-    <button @click="page('prev')">Prev</button>
-    <button @click="page('next')">Next</button>
+    <hr />
+    <div class="scrollable">
+      <router-link v-bind:to="'/banks/' + bank._id" v-for="bank in banks" v-bind:key="bank._id">
+        <div class="bank-slip">
+          <p>{{ bank.memberName }}</p>
+          <p><span class="slip-label">Title: </span>{{ bank.title }}</p>
+          <p><span class="slip-label">Issued On: </span>{{ bank.dateCreated }}</p>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
 export default {
-  data () {
-    return {
-      curBanks: [],
-      curPage: 0,
-      perPage: 5
-    };
-  },
   computed: {
     ...mapGetters([
       'banks'
-    ]),
-    pagedBanks () {
-      const offset = this.curPage * this.perPage;
-      return this.banks
-      .sort((a, b) => a.dateCreated < b.dateCreated)
-      .slice(offset, offset + this.perPage);
-    }
+    ])
   },
   methods: {
     ...mapActions([
       'getAllBanks'
-    ]),
-    page (dir) {
-      if (dir === 'next' && this.curPage < Math.floor(this.banks.length / this.perPage)) {
-        this.curPage++;
-      } else if (dir === 'prev' && this.curPage > 0) {
-        this.curPage--;
-      }
-    }
+    ])
   },
   created () {
     this.getAllBanks();
@@ -55,7 +36,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .bank-feed {
-  width: 30%;
+  width: 33%;
 }
 .bank-slip {
   border: 1px solid #e3e3e3;
@@ -68,5 +49,24 @@ export default {
 a {
   color: #222;
   text-decoration: none;
+}
+.scrollable {
+  height: 500px;
+  overflow: auto;
+}
+::-webkit-scrollbar
+{
+  width: 6px;  /* for vertical scrollbars */
+  height: 12px; /* for horizontal scrollbars */
+}
+
+::-webkit-scrollbar-track
+{
+  background: rgba(0, 0, 0, 0.1);
+}
+
+::-webkit-scrollbar-thumb
+{
+  background: rgba(0, 0, 0, 0.5);
 }
 </style>
