9b0a43fe1f392b05ef0c683b6d0956568de75ab2
kenoteb <kenoteb@onid.oregonstate.edu>
2017-09-14 14:09:08 -0700

    paginate banks feed

--- src/components/BankFeed.vue
+++ src/components/BankFeed.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="bank-feed">
     <h3>Bank Feed</h3>
-    <router-link v-bind:to="'/banks/' + bank._id" v-for="bank in banks" v-bind:key="bank._id">
+    <router-link v-bind:to="'/banks/' + bank._id" v-for="bank in pagedBanks" v-bind:key="bank._id">
       <div class="bank-slip">
         <p>{{ bank.memberName }}</p>
         <p><span class="slip-label">Title: </span>{{ bank.title }}</p>
         <p><span class="slip-label">Issued On: </span>{{ bank.dateCreated }}</p>
       </div>
     </router-link>
+    <button @click="page('prev')">Prev</button>
+    <button @click="page('next')">Next</button>
   </div>
 </template>
 
 <script>
 import { mapGetters, mapActions } from 'vuex';
 export default {
+  data () {
+    return {
+      curBanks: [],
+      curPage: 0,
+      perPage: 5
+    };
+  },
   computed: {
     ...mapGetters([
       'banks'
-    ])
+    ]),
+    pagedBanks () {
+      const offset = this.curPage * this.perPage;
+      return this.banks
+      .sort((a, b) => a.dateCreated < b.dateCreated)
+      .slice(offset, offset + this.perPage);
+    }
   },
   methods: {
     ...mapActions([
       'getAllBanks'
-    ])
+    ]),
+    page (dir) {
+      if (dir === 'next' && this.curPage < Math.floor(this.banks.length / this.perPage)) {
+        this.curPage++;
+      } else if (dir === 'prev' && this.curPage > 0) {
+        this.curPage--;
+      }
+    }
   },
   created () {
     this.getAllBanks();
