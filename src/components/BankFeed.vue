cdbd9cbfcff66e1bb41176ca94b41ad2b1aa65f3
kenoteb <kenoteb@onid.oregonstate.edu>
2017-09-15 11:30:27 -0700

    remove duplicate css

--- src/components/BankFeed.vue
+++ src/components/BankFeed.vue
@@ -44,38 +44,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.bank-feed {
-  width: 33%;
-}
-.bank-slip {
-  border: 1px solid #e3e3e3;
-  margin: 10px;
-  padding: 10px;
-}
-.slip-label {
-  font-weight: bold;
-}
-a {
-  color: #222;
-  text-decoration: none;
-}
-.scrollable {
-  height: 500px;
-  overflow: auto;
-}
-::-webkit-scrollbar
-{
-  width: 6px;  /* for vertical scrollbars */
-  height: 12px; /* for horizontal scrollbars */
-}
-
-::-webkit-scrollbar-track
-{
-  background: rgba(0, 0, 0, 0.1);
-}
-
-::-webkit-scrollbar-thumb
-{
-  background: rgba(0, 0, 0, 0.5);
-}
 </style>
